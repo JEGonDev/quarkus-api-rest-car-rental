f20cacf1656af4eb28f59507aa6dec9622c149be
JEGonDev <juan.dev.gonzalez@gmail.com>
2025-08-11 16:36:39 -0500

    Se implementa tolerancia a fallos en la funcionaldiad  findVehicleByPlate

--- src/main/java/org/jegdev/car_rental/vehicles/infrastructure/persistence/VehicleRepositoryImpl.java
+++ src/main/java/org/jegdev/car_rental/vehicles/infrastructure/persistence/VehicleRepositoryImpl.java
@@ -2,6 +2,9 @@ package org.jegdev.car_rental.vehicles.infrastructure.persistence;
 
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.jegdev.car_rental.vehicles.domain.model.Vehicle;
 import org.jegdev.car_rental.vehicles.domain.repository.VehicleRepository;
 import org.jegdev.car_rental.vehicles.infrastructure.entity.VehicleEntity;
@@ -31,6 +34,9 @@ public class VehicleRepositoryImpl implements VehicleRepository {
     }
 
     @Override
+    @Retry(maxRetries = 3, delay = 200) // Reintenta la operación hasta 3 veces en caso de fallo con un retraso de 200 ms entre intentos
+    @Timeout(200) // Tiempo máximo de espera de 200 ms para la operación
+    @CircuitBreaker(requestVolumeThreshold = 4, failureRatio = 0.75, delay = 1000) // Abre el circuito si el 75% de las últimas 4 llamadas fallan, con un retraso de 1 segundo antes de intentar cerrar el circuito
     public Optional<Vehicle> findByPlate(String plate) {
         return repository.find("plate", plate)
                 .firstResultOptional()
